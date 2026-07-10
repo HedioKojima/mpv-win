85d2fc4153e2b14ca14bad50155e533734f30149
HedioKojima <HedioKojima@users.noreply.github.com>
2026-07-10 00:56:50 +0000

    Use GitHub API for release downloads + fix autoupdate workflow
    Changes:
    - build.yml: replace hardcoded version numbers in download URLs with
      GitHub API calls that fetch the latest asset from each release tag.
      This means autoupdate.py no longer needs to update download URLs in
      build.yml (only the mcfgthread cache key).
    - autoupdate.yml: add 'permissions: contents: write' so the workflow
      can commit changes back to the repo. Use GITHUB_TOKEN instead of BOT
      secret. Only commit if there are actual changes (git diff --quiet).
    - autoupdate.py: simplified - only updates toolchain.yml (mcfgthread
      cache key + download URL) and ffmpeg/mpv PKGBUILD-git pkgver lines.
      No longer touches build.yml download URLs.
    
    Correct workflow order:
    1. autoupdate.yml (updates mcfgthread version + ffmpeg/mpv pkgver)
    2. toolchain.yml (builds MSYS2 + mcfgthread toolchain)
    3. build.yml (builds all 18 components + packages)

--- autoupdate.py
+++ autoupdate.py
@@ -16,39 +16,29 @@ with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
       l = '%s%s.7z --resolve "gcc-mcf.lhmouse.com:443:204.152.213.15"\n' % (l[:i+71], x['Mingw-w64'])
     f.write(l)
 
+# Update build.yml: only the mcfgthread cache key (download URLs use GitHub API now)
+with in_place.InPlace('.github/workflows/build.yml', newline='') as f:
+  for l in f:
+    if (i:=l.find('key: mcf_')) > -1:
+      l = '%s%s\n' % (l[:i+9], mingw)
+    f.write(l)
+
 # mcfgthread version (first 8 chars of mingw build id)
 pkgs = {'mcfgthread': mingw[:8]}
 
 # ffmpeg and mpv pre-release labels (e.g. 8.2pre, 0.42pre)
 # These are derived from the latest stable release version + 1 minor
+# Used in PKGBUILD-git pkgver (makepkg needs a pkgver to name the output package)
 ffmpeg_git = x['ffmpeg'].split('.')[:2]
 mpv_git = x['mpv'].split('.')[:2]
 pkgs['ffmpeg'] = '%s.%dpre' % (ffmpeg_git[0], int(ffmpeg_git[1])+1)
 pkgs['mpv'] = '%s.%dpre' % (mpv_git[0], int(mpv_git[1])+1)
 
 # Update PKGBUILD-git files for ffmpeg and mpv (only the pkgver line)
+# pkgver is used by makepkg to name the output .pkg.tar.zst file
 for p in ['ffmpeg', 'mpv']:
   with in_place.InPlace('%s/PKGBUILD-git' % p, newline='') as f:
     for l in f:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
       f.write(l)
-
-# Update build.yml: mcfgthread cache key + download URLs for ffmpeg/mpv/mcfgthread
-with in_place.InPlace('.github/workflows/build.yml', newline='') as f:
-  for l in f:
-    if (i:=l.find('key: mcf_')) > -1:
-      l = '%s%s\n' % (l[:i+9], mingw)
-    elif (i:=l.find('/bleeding_edge')) > -1:
-      r = l[i+25:]
-      if r.startswith('ffmpeg-git-dev'):
-        l = '%sffmpeg-git-dev-%s-1-x86_64.pkg.tar.zst\n' % (l[0:i+25], pkgs['ffmpeg'])
-      elif r.startswith('ffmpeg-git'):
-        l = '%sffmpeg-git-%s-1-x86_64.pkg.tar.xz\n' % (l[0:i+25], pkgs['ffmpeg'])
-      elif r.startswith('mpv-git'):
-        l = '%smpv-git-%s-1-x86_64.pkg.tar.xz\n' % (l[0:i+25], pkgs['mpv'])
-    elif (i:=l.find('/latest-x86-64-v3')) > -1:
-      r = l[i+18:]
-      if r.startswith('mcfgthread'):
-        l = '%smcfgthread-%s-1-x86_64.pkg.tar.xz\n' % (l[0:i+18], pkgs['mcfgthread'])
-    f.write(l)
